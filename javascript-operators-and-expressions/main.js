704b12d16e3ee87c0a28830804de44aeca319971
Bo Mercado <rbomercado1@gmail.com>
2022-01-31 12:34:54 -0800

    completed js operators and expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+var width = 5;
+var height = 10;
+var area = width * height;
+
+var bill = 8;
+var payment = 20;
+var change = payment - bill;
+
+var quizzes = 80;
+var midterm = 90;
+var final = 85;
+var grade = (quizzes + midterm + final) / 3;
+
+var firstName = 'Bo';
+var lastName = 'Mercado';
+var fullName = firstName + ' ' + lastName;
+
+var pH = 4;
+var isAcidic = (pH < 7);
+
+var headCount = 20;
+var isSparta = (headCount === 300);
+
+var motto = fullName;
+motto = motto + ' ' + 'is the GOAT';
+
+console.log('value of area:' + ' ' + area);
+console.log('typeof area:' + ' ' + typeof area);
+console.log('value of change:' + '' + change);
+console.log('typeof change:' + ' ' + typeof change);
+console.log('value of grade:' + ' ' + grade);
+console.log('typeof grade:' + ' ' + typeof grade);
+console.log('value of fullName:' + ' ' + fullName);
+console.log('type of fullName:' + ' ' + typeof fullName);
+console.log('value of isAcidic:' + ' ' + isAcidic);
+console.log('type of isAcidic:' + ' ' + typeof isAcidic);
+console.log('value of isSparta:' + ' ' + isSparta);
+console.log('type of isSparta:' + ' ' + typeof isSparta);
+console.log('value of motto:' + ' ' + motto);
+console.log('type of motto:' + ' ' + typeof motto);
