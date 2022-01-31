e991c924f25359cde598b3a940ef1cd62add3988
Bo Mercado <rbomercado1@gmail.com>
2022-01-31 15:36:19 -0800

    completd javascript objects

---
+++ javascript-objects/main.js
@@ -0,0 +1,35 @@
+var student = {
+  firstName: 'Bo',
+  lastName: 'Mercado',
+  age: '27'
+};
+var fullName = (student.firstName + ' ' + student.lastName);
+student.livesInIrvine = false;
+student.previousOccupation = 'lab tech';
+
+var vehicle = {
+  make: 'Kia',
+  model: 'Niro',
+  year: '2019'
+};
+
+vehicle.color = 'silver';
+vehicle.isConvertible = false;
+
+var pet = {
+  name: 'Bailey',
+  type: 'dog'
+};
+delete pet.name;
+delete pet.type;
+
+console.log('value of fullName:' + ' ' + fullName);
+console.log('value of student.livesInIrvine:' + ' ' + student.livesInIrvine);
+console.log('value of student.previousOccupation:' + student.previousOccupation);
+console.log(' value of student:', student);
+
+console.log('value of vehicle:' + ' ' + vehicle.color);
+console.log('value of vehicle:' + ' ' + vehicle.isConvertible);
+console.log(' value of vehicle:', vehicle);
+
+console.log(' value of pet:', pet);
