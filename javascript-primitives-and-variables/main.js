74e209c8f74fbe0c93a4ee13e314484f107a8a36
Bo Mercado <rbomercado1@gmail.com>
2022-01-31 10:50:18 -0800

    completed js primitives and variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Richard Bo Mercado';
+var isCool = true;
+var totalPets = 1;
+var unknown = null;
+var never = '';
+
+console.log('Value of fullName:' + ' ' + fullName);
+console.log('Value of isCool:' + ' ' + isCool);
+console.log('Value of totalPets:' + ' ' + totalPets);
+
+console.log('typeof fullName:' + ' ' + typeof fullName);
+console.log('typeof isCool:' + ' ' + typeof isCool);
+console.log('typeof totalPets:' + ' ' + typeof totalPets);
+
+console.log('value of unknown:' + ' ' + unknown);
+console.log('typeof unknown:' + ' ' + typeof unknown);
+
+console.log('value of never:' + ' ' + never);
+console.log('type of never:' + ' ' + typeof never);
