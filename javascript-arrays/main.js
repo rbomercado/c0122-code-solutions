85724734f45a76c33db4ec03854bf681b4833f7e
Bo Mercado <rbomercado1@gmail.com>
2022-01-31 17:09:05 -0800

    completed javascript-arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,21 @@
+var colors = ['red', 'white', 'blue'];
+
+console.log('value of colors[0]' + ' ' + colors[0]);
+console.log('value of colors[1]' + ' ' + colors[1]);
+console.log('value of colors[2]' + ' ' + colors[2]);
+console.log('America is' + ' ' + colors[0] + ' ' + colors[1] + ' ' + colors[2]);
+
+colors[2] = 'green';
+
+console.log('Mexico is' + ' ' + colors[0] + ' ' + colors[1] + ' ' + colors[2]);
+
+console.log('value of colors:' + ' ' + colors);
+
+var students = ['Jane Hernandez', 'Josh Cricket', 'Nelson Aguilar', ' James Trammel'];
+
+var numberOfStudents = students.length;
+console.log('There are' + ' ' + numberOfStudents + ' students in the class');
+var lastIndex = students[students.length - 1];
+var lastStudent = lastIndex;
+console.log('The last student in the class is' + ' ' + lastStudent);
+console.log('value of students:' + students);
