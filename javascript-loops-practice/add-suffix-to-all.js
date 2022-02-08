87e2cf3219eec66920e06f961e6c19662591e4d5
Bo Mercado <rbomercado1@gmail.com>
2022-02-08 07:14:42 -0800

    updated addsuffixtoall

--- javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -2,7 +2,8 @@
 function addSuffixToAll(words, suffix) {
   var suffixArray = [];
   for (var i = 0; i < words.length; i++) {
-    suffixArray[i] = (words[i] += suffix);
+    suffixArray.push(words[i]);
+    suffixArray[i] = (suffixArray[i] += suffix);
   }
   return suffixArray;
 }
