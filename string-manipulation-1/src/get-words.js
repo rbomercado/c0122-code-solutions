a31f876b23dd578fe87edc14566607588c606020
Bo Mercado <rbomercado1@gmail.com>
2022-02-08 14:27:01 -0800

    string-manipulation-1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-cond-assign */
-/* eslint-disable no-constant-condition */
 /* exported getWords */
 /*
 make a function that splits a string based on spaces and commas
@@ -8,11 +6,5 @@ output and retrive the array of strings with oriignal string split
 
 */
 function getWords(string) {
-  // eslint-disable-next-line no-cond-assign
-  // eslint-disable-next-line no-undef
-  if (word = '') {
-    return string.split('');
-  } else {
-    return string.split(' ', '');
-  }
+  return string.split(/[' ','']/);
 }
