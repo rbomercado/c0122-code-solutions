c206f1f3524ff8280185c3f666dcad9e68db8eb8
Bo Mercado <rbomercado1@gmail.com>
2022-02-09 09:58:40 -0800

    updated var to include elemtns prop

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -4,9 +4,9 @@ contactForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  var name = contactForm.name.value;
-  var email = contactForm.email.value;
-  var message = contactForm.message.value;
+  var name = contactForm.elements.name.value;
+  var email = contactForm.elements.email.value;
+  var message = contactForm.elements.message.value;
   var data = {
     name: name,
     email: email,
