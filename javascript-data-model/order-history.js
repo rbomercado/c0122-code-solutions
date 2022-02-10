6e1063c9d4da1b04dc97794f4489d77c9da29903
Bo Mercado <rbomercado1@gmail.com>
2022-02-09 17:31:06 -0800

    updated item:

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,5 +1,5 @@
 
-var orderHistory = ([
+var orderHistory = [
   {
     orderPlaced: 'Auguest 4, 2020',
     total: 34.00,
@@ -7,20 +7,21 @@ var orderHistory = ([
     orderNum: '114-3941689-87722232',
     deliveredDate: 'Delievered Aug,8 2020',
     deliveryMethod: ' ',
-    item: {
-      description1: ['Javascript for Impatient programmers'],
-      author: ['Rauschmayer Dr Axel'],
-      rtrnWdindow: ['Sep 7,2020'],
-      price: [31.00],
-      buyItAgain: [true],
-      viewItem: [true],
-      productReview: [true],
-      orderRetails: [true],
-      invocie: [true],
-      productSupport: [false]
-    }
+    item: [
+      {
+        description: 'Javascript for Impatient programmers',
+        author: 'Rauschmayer Dr Axel',
+        rtrnWdindow: 'Sep 7,2020',
+        price: 31.00,
+        buyItAgain: true,
+        viewItem: true,
+        productReview: true,
+        orderRetails: true,
+        invoice: true,
+        productSupport: false
+      }
+    ]
   },
-
   {
     orderPlaced: 'July 19,2020',
     total: 44.53,
@@ -28,20 +29,21 @@ var orderHistory = ([
     orderNum: '133-9984268-1280257',
     deliveredDate: 'Delievered Jul 20, 2020',
     deliveryMethod: 'Your package was delivered It was handed directly handed to a resident',
-    item: {
-      description: ['The Timeless Way of Building'],
-      author: ['Alexander Christopher'],
-      rtrnWindow: ['Aug 19,2020'],
-      price: [41.33],
-      buyItAgain: [true],
-      viewItem: [true],
-      productReview: [true],
-      orderDetails: [true],
-      invocie: [true],
-      productSupport: [false]
-    }
+    item: [
+      {
+        description: 'The Timeless Way of Building',
+        author: 'Alexander Christopher',
+        rtrnWindow: 'Aug 19,2020',
+        price: 41.33,
+        buyItAgain: true,
+        viewItem: true,
+        productReview: true,
+        orderDetails: true,
+        invoice: true,
+        productSupport: false
+      }
+    ]
   },
-
   {
     orderPlaced: 'July 4,2020',
     total: 17.22,
@@ -49,18 +51,20 @@ var orderHistory = ([
     orderNum: '114-2875557-9059409',
     deliveredDate: 'Delievered Jul 7, 2020',
     deliveryMethod: 'Your package was delivered.It was handed directly to a resident',
-    item: {
-      description1: ['Gamecube Contorller Adapter.Super Smash Bros Switch Gamecube Adapter fow Wii U Pc.Support Turbo and Vibration features.No Driver and No Lag-Gamecube Adapter'],
-      author: [' '],
-      rtrnWindow: ['Aug 5,2020'],
-      price: [15.98],
-      buyItAgain: [true],
-      viewYourItem: [true],
-      productReview: [true],
-      orderDetails: [true],
-      invocie: [true],
-      productSupprt: [false]
-    }
+    item: [
+      {
+        description: 'Gamecube Contorller Adapter.Super Smash Bros Switch Gamecube Adapter fow Wii U Pc.Support Turbo and Vibration features.No Driver and No Lag-Gamecube Adapter',
+        author: ' ',
+        rtrnWindow: 'Aug 5,2020',
+        price: 15.98,
+        buyItAgain: true,
+        viewYourItem: true,
+        productReview: true,
+        orderDetails: true,
+        invoice: true,
+        productSupprt: false
+      }
+    ]
   },
 
   {
@@ -70,19 +74,33 @@ var orderHistory = ([
     orderNum: '113-2883177-2648248',
     deliveredDate: 'Delievered Jul 5, 2020',
     deliveryMethod: ' ',
-    item: {
-      description1: ['Gamecube Contorller-Super Smash Bros.edition (Nintendo Switch) Pc Support Turbo and Vibration features No Driver and No Lag -Gamecube Adapter', 'the Art of Sql'],
-      author: ['Faroult Stephane'],
-      returnWindow: ['Aug 4,2020'],
-      price: [94.55, 33.99],
-      buyItAgain: [true],
-      viewYourItem: [true],
-      productReview: [true],
-      orderDetails: [true],
-      invocie: [true],
-      productSupport: [true]
-    }
+    item: [
+      {
+        description: 'Gamecube Contorller-Super Smash Bros.edition (Nintendo Switch) Pc Support Turbo and Vibration features No Driver and No Lag -Gamecube Adapter',
+        author: 'Faroult Stephane',
+        returnWindow: 'Aug 4,2020',
+        price: 94.55,
+        buyItAgain: true,
+        viewYourItem: true,
+        productReview: true,
+        orderDetails: true,
+        invoice: true,
+        productSupport: true
+      },
+      {
+        description: 'the Art of Sql',
+        author: 'Faroult Stephane',
+        returnWindow: 'Aug 4,2020',
+        price: 33.99,
+        buyItAgain: true,
+        viewYourItem: true,
+        productReview: true,
+        orderDetails: true,
+        invoice: true,
+        productSupport: true
+      }
+    ]
   }
-]);
+];
 
 console.log(orderHistory);
