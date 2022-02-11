c39a16a3f8b31251222a2a42c92c7807145e7cd9
Bo Mercado <rbomercado1@gmail.com>
2022-02-11 12:34:07 -0800

    in progress pseudo code

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,4 +1,15 @@
 /* exported ransomCase */
+
+/*
+//function with 1 parameter that will capitalize every other word
+input a string
+output new string (need a var for new string)
+loop through each character 1 by 1
+if the i is not divisible by 2 then uppercase add to new string
+if the i is anything else then lowercase add to new string
+return the new string
+*/
+
 function ransomCase(string) {
   var newString = '';
   for (var i = 0; i < string.length; i++) {
--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,4 +1,14 @@
 /* exported truncate */
+/*
+function with 2 parameters
+input will be a length and a string
+output will be a shortned string with ellip addded to end
+need a var for ellip
+need a variable for the new string
+if the string is qual to an empty string only return an empty string
+everything else we want to cut the string at length para and add ellip to the end
+*/
+
 function truncate(length, string) {
   var ellip = '...';
   var emptyString = '';
