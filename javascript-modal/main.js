d20b8bf94567aa42f889c03624830b02e92222c1
Bo Mercado <rbomercado1@gmail.com>
2022-02-11 23:35:50 -0800

    completed js modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,4 +1,27 @@
-// var buttonClicked = document.querySelector('.open-button');
-// buttonClicked.addEventListener('click', modalDrop);
+var activate = true;
+var openModal = document.querySelector('.open-modal-button');
+var modalShow = document.querySelector('.container');
+var closeModal = document.querySelector('.no-button');
 
-// function modalDrop
+openModal.addEventListener('click', activateModal);
+function activateModal(event) {
+  if (activate === true) {
+    openModal.className = 'open-modal-button hidden';
+    modalShow.className = 'container show';
+    activate = false;
+  } else {
+    activate = true;
+  }
+  return activateModal;
+}
+
+closeModal.addEventListener('click', deActivateModal);
+function deActivateModal(event) {
+  if (activate === true) {
+    modalShow.className = 'container hidden';
+    activate = false;
+  } else {
+    activate = true;
+  }
+  return deActivateModal;
+}
