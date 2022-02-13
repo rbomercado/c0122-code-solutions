81f68a07a090ca8d1f2cd1c062be66f80198c0bb
Bo Mercado <rbomercado1@gmail.com>
2022-02-12 18:32:08 -0800

    completed js typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,8 +1,6 @@
-
 var spans = document.querySelectorAll('span');
 document.addEventListener('keydown', keyPushed);
 var i = 0;
-
 function keyPushed(event) {
   if (event.key === spans[i].textContent) {
     spans[i].className = 'correct';
