8860c6ad5f9a16fe5535700632d3de40e98bb04f
Bo Mercado <rbomercado1@gmail.com>
2022-02-14 11:59:49 -0800

    updated if statement to be outside of funct

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,12 +1,12 @@
 /* exported todos */
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
 
 window.addEventListener('beforeunload', beforeUnload);
 function beforeUnload(event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
-  if (previousTodosJSON !== null) {
-    todos = JSON.parse(previousTodosJSON);
-  }
 }
